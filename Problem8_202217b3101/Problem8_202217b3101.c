6a3f3455c7794315501aa8abbd24d3c7a3788336
KanavSharma123 <202217B3101@wilp.bits-pilani.ac.in>
2024-07-20 23:20:19 +0530

    Solved Problem 8

--- Problem8_202217b3101/Problem8_202217b3101.c
+++ Problem8_202217b3101/Problem8_202217b3101.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
-void bubble_sort(int arr[], int n)
+void selection_sort(int arr[], int n)
 {
-    int i, j;
-    for (i = 0; i < n - 1; i++)
+    int pos, small;
+    for (int i = 0; i < n - 1; i++)
     {
-        for (j = 0; j < n - i - 1; j++)
+        small = arr[i];
+        pos = i;
+        for (int j = i + 1; j < n; j++)
         {
-            if (arr[j] > arr[j + 1])
+            if (arr[j] < small)
             {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                small = arr[j];
+                pos = j;
             }
         }
+        int temp = arr[i];
+        arr[i] = arr[pos];
+        arr[pos] = temp;
     }
 }
 int main()
 {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
-    bubble_sort(arr, n);
+    selection_sort(arr, n);
     printf("Sorted array: ");
     for (int i = 0; i < n; i++)
     {
